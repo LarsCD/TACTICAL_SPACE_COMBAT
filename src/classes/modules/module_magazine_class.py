fbe94ebcce052e42f89d12a793a620922e189982
LarsCD <larscdijkgraaf@gmail.com>
2024-01-06 01:42:34 +0100

    added content to modules

--- src/classes/modules/module_magazine_class.py
+++ src/classes/modules/module_magazine_class.py
@@ -10,3 +10,14 @@ class Module_Magazine:
         # stats
         self.ammo_hold = stats['amount']
         self.ammo_type = stats['ammo_type']
+
+        # hp stats
+        self.hp_max = stats['hp_max']
+        self.hp_current = stats['hp_current']
+        self.hp_percentage = 0
+
+        # call init methods
+        self.update_hp_percentage()
+
+    def update_hp_percentage(self, round_nr=1):
+        self.hp_percentage = round(((self.hp_current / self.hp_max) / 100), round_nr)
--- src/classes/modules/module_weapon_class.py
+++ src/classes/modules/module_weapon_class.py
@@ -1,4 +1,3 @@
-
 class Module_Weapon:
     def __init__(self, name: dict, stats: dict):
         # name
@@ -12,9 +11,17 @@ class Module_Weapon:
         self.damage_type = stats['damage_type']
 
         # hp stats
-        self.hp = stats['hp']
+        self.hp_max = stats['hp_max']
+        self.hp_current = stats['hp_current']
+        self.hp_percentage = 0
 
         # ammo stats
         self.ammo_hold = stats['ammo_hold']
         self.ammo_type = stats['ammo_type']
         self.reload_time = stats['reload_time']
+
+        # call init methods
+        self.update_hp_percentage()
+
+    def update_hp_percentage(self, round_nr=1):
+        self.hp_percentage = round(((self.hp_current / self.hp_max) / 100), round_nr)
